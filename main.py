10555732eaa6634b66a569cf3edbdcf72794737c
Behzad Pirvali <bpirvali@gmail.com>
2023-03-19 09:21:21 -0500

    added another print line

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('GitHub Actions from PyCharm')
-
+    print("Using the actions-template:'test python app'")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
