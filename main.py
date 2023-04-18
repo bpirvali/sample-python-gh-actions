b620342ae3e4d9cc9ebe8d7dd80a1848db51e094
Behzad Pirvali <bpirvali@gmail.com>
2023-04-18 09:12:27 -0500

    added a line to cick-off actions

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('GitHub Actions from PyCharm')
     print("Using the actions-template:'test python app'")
     print("This line was added to a feature branch")
+    print("This line is added to run git actions!")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
